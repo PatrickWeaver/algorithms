adce6793bb7971a8bb91888743d144392eff272e
Patrick Weaver <pjpwea@gmail.com>
2018-05-12 19:57:46 -0400

    finish merge sort

--- python/sort.py
+++ python/sort.py
@@ -1,5 +1,5 @@
 import random_numbers
-
+import math
 
 def insertion(number_list):
   for j in range(1, len(number_list)):
@@ -14,29 +14,41 @@ def insertion(number_list):
 def merge(number_list):
 
   def merge_lists(number_list, p, q, r):
-    print(number_list)
     n1 = q - p + 1
     n2 = r - q
-    l = [None] * (n1 + 1)
-    r = [None] * (n2 + 1)
+    left = [math.inf] * (n1 + 1)
+    right = [math.inf] * (n2 + 1)
 
     for i in range(0, n1):
-      l[i] = number_list[p + i - 1]
+      left[i] = number_list[p + i]
 
     for j in range(0, n2):
-      r[j] = number_list[q + j]
+      right[j] = number_list[q + j + 1]
     
-    print(l)
-    print(r)
+    i = 0
+    j = 0
+
+    for k in range(p, r + 1):
+      if left[i] <= right[j]:
+        number_list[k] = left[i]
+        i = i + 1
+      else:
+        number_list[k] = right[j]
+        j = j + 1
+
+  def merge_sort(number_list, p, r, o):
+    if p < r:
+      q = (p + r) // 2
+      merge_sort(number_list, p, q, 1)
+      merge_sort(number_list, q + 1, r, 2)
+      merge_lists(number_list, p, q, r)
+
+  merge_sort(number_list, 0, len(number_list) - 1, 0)
 
 
-
-  n = [0,1,2,3,4,5,6,7]
-  merge_lists(n, 0, 3, 7)
-
-  pass
-
-
-merge([])
+random_list = random_numbers.random_integer_list()
+print(random_list)
+merge(random_list)
+print(random_list)
 
 
