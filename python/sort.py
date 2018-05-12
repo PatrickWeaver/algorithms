f068646def136686ca2fd1f47136d5070128ac55
Patrick Weaver <pjpwea@gmail.com>
2018-05-12 10:07:36 -0400

    halfway merge sort

--- python/sort.py
+++ python/sort.py
@@ -11,11 +11,32 @@ def insertion(number_list):
       number_list[i + 1] = k
   return number_list  
 
+def merge(number_list):
+
+  def merge_lists(number_list, p, q, r):
+    print(number_list)
+    n1 = q - p + 1
+    n2 = r - q
+    l = [None] * (n1 + 1)
+    r = [None] * (n2 + 1)
+
+    for i in range(0, n1):
+      l[i] = number_list[p + i - 1]
+
+    for j in range(0, n2):
+      r[j] = number_list[q + j]
+    
+    print(l)
+    print(r)
 
 
-random_list = random_numbers.random_integer_list()
-print("Random List:")
-print(random_list)
-sorted_list = insertion(random_list)
-print("Sorted List:")
-print(sorted_list)
+
+  n = [0,1,2,3,4,5,6,7]
+  merge_lists(n, 0, 3, 7)
+
+  pass
+
+
+merge([])
+
+
